7dc2fbf56320e9f4917b005237c1e90e4b6bb827
asparkoffire <kabir.47.chd@gmail.com>
2025-05-19 21:09:44 +0530

    feat: better randmoji loop

--- internal/services/messagehandler/memes_and_fun.go
+++ internal/services/messagehandler/memes_and_fun.go
@@ -17,7 +17,7 @@ func getRandomEmoji() string {
 	return constants.Emojis[rng.Intn(len(constants.Emojis))]
 }
 
-func randomEmoji(h *WhatsMeowEventHandler, msgInfo types.MessageInfo, duration int) {
+func randomEmoji(h *WhatsMeowEventHandler, msgInfo types.MessageInfo, times int) {
 	// Try to set the flag from 0 to 1
 	if !atomic.CompareAndSwapInt32(&randmojiRunning, 0, 1) {
 		// Already running
@@ -26,13 +26,12 @@ func randomEmoji(h *WhatsMeowEventHandler, msgInfo types.MessageInfo, duration i
 	}
 	defer atomic.StoreInt32(&randmojiRunning, 0)
 
-	log.Printf("invoking randmoji routine with duration: %d seconds\n", duration)
-	start := time.Now()
-	for {
-		if time.Since(start) > time.Duration(duration)*time.Second {
-			return
+	log.Printf("invoking randmoji routine with loop count: %d\n", times)
+
+	for range times {
+		for range 3 {
+			time.Sleep(time.Millisecond * 500)
+			h.editMessageContent(msgInfo.Chat, msgInfo.ID, getRandomEmoji(), nil)
 		}
-		time.Sleep(time.Millisecond * 500)
-		h.editMessageContent(msgInfo.Chat, msgInfo.ID, getRandomEmoji(), nil)
 	}
 }
